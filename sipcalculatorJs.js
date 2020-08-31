3a1661e7711fd538064d4b9f998332e88e4f8387
vengatesan_t <vengatesan.t@bridgei2i.com>
2020-08-31 18:02:37 +0530

    second commit

--- sipcalculatorJs.js
+++ sipcalculatorJs.js
@@ -2,16 +2,32 @@ calculateResult=()=>{
     let amountValue = document.getElementById("investment").value;
     let amountWithComma = (amountValue.split(" ")[0]);
     let amount = parseInt(amountWithComma.split(',').join(''));
+    document.getElementById("input-1").innerHTML = isNaN(amount)?0:amount;
 
     let years = document.getElementById("years").value;
+
+    document.getElementById("input-2").innerHTML = years.length===0?"0":years;
+
 
     let returnRateValue = document.getElementById("return-rate").value;
     let returnRateWithComma = (returnRateValue.split(" ")[0]);
     let returnRate = parseInt(returnRateWithComma.split(',').join(''));
+    document.getElementById("input-3").innerHTML = isNaN(returnRate)?0:returnRate;
 
     let checkedValue = document.getElementsByName('checked')[0].checked;
     let wealthGained = 0; total = 0;maturityValue = 0;
 
+    if(document.getElementsByName('checked')[0].checked){
+        document.getElementById("mode-1").innerHTML = document.getElementsByName('checked')[0].value;
+        document.getElementById("mode-2").innerHTML = document.getElementsByName('checked')[0].value;
+        document.getElementById("mode-3").innerHTML = document.getElementsByName('checked')[0].value;
+
+    }
+    if(document.getElementsByName('checked')[1].checked){
+        document.getElementById("mode-1").innerHTML = document.getElementsByName('checked')[1].value;
+        document.getElementById("mode-2").innerHTML = document.getElementsByName('checked')[1].value;
+        document.getElementById("mode-3").innerHTML = document.getElementsByName('checked')[1].value;
+    }
     if(checkedValue){
         wealthGained = Math.round((Math.pow((1 + (Math.pow((1 + returnRate / 100), (1 / 12)) - 1)), (years * 12)) - 1) / (Math.pow((1+ returnRate / 100), (1 / 12)) - 1) * amount);
         total = (amount*12)*years; 
@@ -27,10 +43,13 @@ calculateResult=()=>{
     wealthGained = wealthGained.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     maturityValue = maturityValue.toString();
     maturityValue = maturityValue.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-    document.getElementById("total").innerHTML =  total;
-    document.getElementById("wealth-gained").innerHTML = wealthGained;
-    document.getElementById("maturity-value").innerHTML =  maturityValue;
+    document.getElementById("investment").value = ''
+    document.getElementById("years").value = '';
+    document.getElementById("return-rate").value = '';
+    console.log(total,typeof total  )
+    document.getElementById("total").innerHTML = total==="NaN"?"0":total;
+    document.getElementById("wealth-gained").innerHTML = wealthGained==="NaN"?"0":wealthGained;
+    document.getElementById("maturity-value").innerHTML = maturityValue==="NaN"?"0":maturityValue
 }
 
 currencyChange=()=>{
